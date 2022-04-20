3d4d288f4db13e8d6ff8de35856a203f8863b336
sibulele <smboyi2016@gmail.com>
2022-04-20 15:37:08 +0200

    creating email content

--- src/main/java/com/springsecurity/supportportal/services/EmailService.java
+++ src/main/java/com/springsecurity/supportportal/services/EmailService.java
@@ -2,14 +2,34 @@ package com.springsecurity.supportportal.services;
 
 import static com.springsecurity.supportportal.constants.EmailConstants.*;
 
+import java.util.Date;
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
+    private Message createEmail(String firstName, String password, String email) throws AddressException, MessagingException{
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        
+        message.setText("Hello "+ firstName + " \n \n  Your new account passsword is: "+ password + "\n \n The Support Team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+        
+    }
     private Session getEmailSession(){
         Properties properties = System.getProperties();
         properties.put(SMTP_HOST, GMAIL_SMTP_SERVER);
